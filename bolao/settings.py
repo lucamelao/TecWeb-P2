d4ea64a9db5747bc9e281c31c275baf7b6032571
pauloschade <paulosc4@al.insper.edu.br>
2021-12-01 16:42:18 -0300

    keys upadated

--- bolao/settings.py
+++ bolao/settings.py
@@ -77,20 +77,20 @@ WSGI_APPLICATION = 'bolao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://localhost/bolao?user=bolaouser&password=bolaopassword',
-#         conn_max_age=600,
-#         ssl_require=not DEBUG
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://localhost/bolao?user=bolaouser&password=bolaopassword',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
+}
 
 
 # Password validation
